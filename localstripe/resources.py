6a0ef544212c536918db0dbe4c9aeecf0d3d08f8
Victor Quimson <victor.quimson@wagwalking.com>
2025-09-03 14:20:42 -1000

    add mock response for v1/payment_methods

--- localstripe/resources.py
+++ localstripe/resources.py
@@ -2262,7 +2262,8 @@ class PaymentMethod(StripeObject):
 
 extra_apis.extend((
     ('POST', '/v1/payment_methods/{id}/attach', PaymentMethod._api_attach),
-    ('POST', '/v1/payment_methods/{id}/detach', PaymentMethod._api_detach)))
+    ('POST', '/v1/payment_methods/{id}/detach', PaymentMethod._api_detach),
+    ('POST', '/v1/payment_methods', PaymentMethod._api_list_all)))
 
 
 class Plan(StripeObject):
